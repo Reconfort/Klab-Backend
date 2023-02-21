2cff22081992f81f1c639858449772e046594936
Reconfort Daniel <danny.reco2002@gmail.com>
2023-02-21 23:29:56 +0200

    Comment unnecessary Codes in dotenv

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import dotenv from "dotenv";
+import router from '../routes/index';
 
 dotenv.config();
 
 const app = express()
+
+app.use(router);
 
 app.listen(process.env.PORT, () => {
     console.log(`App is listerning to ${process.env.PORT}`)
